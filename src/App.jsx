969177339af339a907522cd377617e261eb562a2
yujinKim <yujii22001@gmail.com>
2025-05-25 03:59:43 +0900

    feat: 페이지 라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/admin" element={<Admin />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="/category" element={<Home />} />
             <Route path="/price" element={<Home />} />
             <Route path="/sort" element={<Home />} />
