15d0341eb5468b7f18cc8b95af34c12a8d465465
yujinKim <yujii22001@gmail.com>
2025-05-25 02:22:27 +0900

    feat: tw-merge 스타일 적용

--- src/components/BlueButton.jsx
+++ src/components/BlueButton.jsx
@@ -1,7 +1,15 @@
-const BlueButton = ({ text }) => {
+// twMerge 라이브러리 설치 필요
+import { twMerge } from "tailwind-merge";
+
+const BlueButton = ({ text, className = "" }) => {
   return (
     <>
-      <button className="flex h-[43px] w-[148px] items-center justify-center rounded-[8px] bg-[#008CFF] px-[16px] py-[12px] text-white hover:cursor-pointer">
+      <button
+        className={twMerge(
+          "flex h-[43px] w-[148px] items-center justify-center rounded-[8px] bg-[#008CFF] px-[16px] py-[12px] text-white hover:cursor-pointer",
+          className
+        )}
+      >
         {text}
       </button>
     </>
