1e471d442beb71b6b9d145b13029498f97f55f57
yujinKim <yujii22001@gmail.com>
2025-05-25 02:22:57 +0900

    feat: 관리자 페이지

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,30 @@
+import AdminNavbar from "../components/navbar/AdminNavbar";
+import StockBox from "../components/StockBox";
+
 const Admin = () => {
-  return <>나는 관리자 페이지!</>;
+  return (
+    <>
+      <AdminNavbar />
+      <div class="mt-[132px] w-[652px]">
+        <section className="flex flex-col gap-[28px]">
+          <div className="text-[20px] font-[600]">상품 등록</div>
+          <StockBox section2Exist={true} comment={true} />
+        </section>
+
+        <section className="flex flex-col gap-[28px]">
+          <div className="text-[20px] font-[600]">재고 추가</div>
+          <StockBox />
+        </section>
+
+        <section className="flex flex-col gap-[28px]">
+          <div className="text-[20px] font-[600]">상품 삭제</div>
+          <StockBox stockExist={false} />
+        </section>
+
+        <div className="h-[100px]"></div>
+      </div>
+    </>
+  );
 };
 
 export default Admin;
