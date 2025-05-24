f6f3b2e7bec24bf98a710348f8c51462f7abf56a
yujinKim <yujii22001@gmail.com>
2025-05-25 04:01:41 +0900

    feat: 페이지 이동 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,11 @@ import CategoryFilter from "../components/Contents/CategoryFilter";
 import PriceFilter from "../components/Contents/PriceFilter";
 import ItemSort from "../components/Contents/ItemSort";
 import DefaultContents from "../components/Contents/DefaultContents";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Home = () => {
   const location = useLocation();
+  const nav = useNavigate();
 
   // 메뉴바 선택에 따른 콘텐츠 렌더링
   const renderContent = () => {
@@ -27,7 +28,10 @@ const Home = () => {
       <UserNavbar />
       {renderContent()}
 
-      <button className="fixed bottom-[40px] w-[652px] rounded-[8px] border-[2px] border-[#008CFF] bg-white px-[16px] py-[12px] text-[#008CFF] hover:cursor-pointer">
+      <button
+        onClick={() => nav("/cart")}
+        className="fixed bottom-[40px] w-[652px] rounded-[8px] border-[2px] border-[#008CFF] bg-white px-[16px] py-[12px] text-[#008CFF] hover:cursor-pointer"
+      >
         장바구니 구매하기
       </button>
     </>
