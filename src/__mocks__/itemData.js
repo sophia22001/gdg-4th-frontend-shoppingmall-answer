9a95450e359dd290856b8a3ab72f1a9129d4fc0e
yujinKim <yujii22001@gmail.com>
2025-05-25 01:14:54 +0900

    feat: 임시 데이터 추가

---
+++ src/__mocks__/itemData.js
@@ -0,0 +1,7 @@
+// mockData
+export const itemData = [
+  { id: 1, name: "상품1", price: 1000, stock: 13 },
+  { id: 2, name: "상품2", price: 200, stock: 7 },
+  { id: 3, name: "상품3", price: 5000, stock: 14 },
+  { id: 4, name: "상품4", price: 8000, stock: 113 },
+];
