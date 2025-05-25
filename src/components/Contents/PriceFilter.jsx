14bc0702a807e027eaa397cb03f1677db17565d3
yujin <127318207+sophia22001@users.noreply.github.com>
2025-05-26 01:05:33 +0900

    변경된 화면 레이아웃 코드 적용하기  (#1)
    * refactor: 검색 코드 개선
    
    * chore: 설정 느슨하게
    
    * refactor: 토글 콘텐츠 페이지 개선
    
    * refactor: 가격 필터링 코드 개선
    
    * feat: mockData 추가
    
    * chore: 주석 삭제
    
    * feat: 불필요한 코드 삭제
    
    * feat: num type 설정
    
    * chore: 변수 이름 수정

--- src/components/Contents/PriceFilter.jsx
+++ src/components/Contents/PriceFilter.jsx
@@ -1,26 +1,54 @@
-import { itemData } from "../../__mocks__/itemData";
+import { useState } from "react";
+import { priceSelectedData } from "../../__mocks__/itemData";
 import GDGlogo from "../../assets/gdg-background.png";
 import PriceBracketInput from "../input-bar/PriceBracketInput";
 import ItemList from "../ItemList";
-import UserNavbar from "../navbar/UserNavbar";
+import { useLocation } from "react-router-dom";
 
 const PriceFilter = () => {
+  const location = useLocation();
+  // 검색된 가격
+  const [lowPrice, setLowPrice] = useState(0);
+  const [highPrice, setHighPrice] = useState(0);
+
+  // 검색된 데이터 배열
+  const [priceResult, setPriceResult] = useState(priceSelectedData.items);
+
+  const showItems = location.pathname === "/price" ? priceResult : [];
+  const isEmpty = showItems.length === 0;
+
+  function handleSearch(low, high) {
+    console.log("검색 클릭됨");
+    console.log(low, high);
+  }
+
   return (
     <>
       <div className="mt-[60px] w-[652px]">
-        <PriceBracketInput />
-        {itemData.length > 0 ? (
-          <div className="mt-[72px] mb-[200px]">
-            <ItemList />
-          </div>
-        ) : (
-          <div className="absolute top-1/2 left-1/2 block h-[300px] w-[300px] -translate-x-1/2 -translate-y-1/2 transform">
-            <img src={GDGlogo} style={{ opacity: 0.15 }} />
-            <div className="absolute top-[80%] flex w-full justify-center text-[16px] text-[#8F8F8F]">
-              검색 결과가 없습니다.
+        <PriceBracketInput
+          setLowPrice={setLowPrice}
+          setHighPrice={setHighPrice}
+          lowPrice={lowPrice}
+          highPrice={highPrice}
+          handleSearch={() =>
+            handleSearch(priceSelectedData.low, priceSelectedData.high)
+          }
+        />
+
+        <div>
+          {isEmpty ? (
+            <div className="absolute top-1/2 left-1/2 block h-[300px] w-[300px] -translate-x-1/2 -translate-y-1/2 transform">
+              <img src={GDGlogo} style={{ opacity: 0.15 }} />
+              <div className="absolute top-[80%] flex w-full justify-center text-[16px] text-[#8F8F8F]">
+                검색 결과가 없습니다.
+              </div>
             </div>
-          </div>
-        )}
+          ) : (
+            <div className="mt-[72px] mb-[200px]">
+              <ItemList items={showItems} />
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
--- src/components/input-bar/SearchInput.jsx
+++ src/components/input-bar/SearchInput.jsx
@@ -2,15 +2,17 @@
 
 import BlueButton from "../BlueButton";
 
-const SearchInput = () => {
+const SearchInput = ({ query, setQuery, handleSearch }) => {
   return (
     <>
       <div className="mt-[72px] flex w-full items-center gap-[20px]">
         <input
+          value={query}
+          onChange={e => setQuery(e.target.value)}
           className="h-[43px] w-[484px] rounded-[8px] border border-[#8F8F8F] px-[16px] py-[12px]"
           placeholder="상품 검색..."
         />
-        <BlueButton text={"검색"} />
+        <BlueButton text={"검색"} onClick={() => handleSearch()} />
       </div>
     </>
   );
