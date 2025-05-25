b070089639cdf86703165cdda1e32a645c71135f
yujinKim <yujii22001@gmail.com>
2025-05-26 00:47:40 +0900

    refactor: 가격 필터링 코드 개선

--- src/components/Contents/PriceFilter.jsx
+++ src/components/Contents/PriceFilter.jsx
@@ -1,17 +1,61 @@
-import { itemData } from "../../__mocks__/itemData";
+import { useState } from "react";
+import { priceSelectedData } from "../../__mocks__/itemData";
 import GDGlogo from "../../assets/gdg-background.png";
 import PriceBracketInput from "../input-bar/PriceBracketInput";
 import ItemList from "../ItemList";
-import UserNavbar from "../navbar/UserNavbar";
+import { useLocation } from "react-router-dom";
 
 const PriceFilter = () => {
+  const location = useLocation();
+  // 검색된 가격
+  const [lowPrice, setLowPrice] = useState(0);
+  const [highPrice, setHighPrice] = useState(0);
+
+  // 검색된 데이터 배열
+  const [priceResult, setPriceResult] = useState(priceSelectedData.items);
+
+  // 검색 버튼이 클릭되었는지. 검색 여부
+  const [searchClicked, setSearchClicked] = useState(false);
+
+  const showItems = location.pathname === "/price" ? priceResult : [];
+  const isEmpty = showItems.length === 0;
+
+  function handleSearch(low, high) {
+    console.log("검색 클릭됨");
+    setSearchClicked(true);
+    console.log(low, high);
+  }
+
   return (
     <>
       <div className="mt-[60px] w-[652px]">
-        <PriceBracketInput />
-        {itemData.length > 0 ? (
+        <PriceBracketInput
+          setLowPrice={setLowPrice}
+          setHighPrice={setHighPrice}
+          lowPrice={lowPrice}
+          highPrice={highPrice}
+          handleSearch={() =>
+            handleSearch(priceSelectedData.low, priceSelectedData.high)
+          }
+        />
+
+        <div>
+          {isEmpty ? (
+            <div className="absolute top-1/2 left-1/2 block h-[300px] w-[300px] -translate-x-1/2 -translate-y-1/2 transform">
+              <img src={GDGlogo} style={{ opacity: 0.15 }} />
+              <div className="absolute top-[80%] flex w-full justify-center text-[16px] text-[#8F8F8F]">
+                검색 결과가 없습니다.
+              </div>
+            </div>
+          ) : (
+            <div className="mt-[72px] mb-[200px]">
+              <ItemList items={showItems} />
+            </div>
+          )}
+        </div>
+        {/* {itemData.length > 0 ? (
           <div className="mt-[72px] mb-[200px]">
-            <ItemList />
+            <ItemList items={priceSelectedData} />
           </div>
         ) : (
           <div className="absolute top-1/2 left-1/2 block h-[300px] w-[300px] -translate-x-1/2 -translate-y-1/2 transform">
@@ -20,7 +64,7 @@ const PriceFilter = () => {
               검색 결과가 없습니다.
             </div>
           </div>
-        )}
+        )} */}
       </div>
     </>
   );
--- src/components/input-bar/PriceBracketInput.jsx
+++ src/components/input-bar/PriceBracketInput.jsx
@@ -1,20 +1,36 @@
 // 가격 범위 필터링 페이지에서 쓰일 입력 창
 
+import { useEffect } from "react";
 import BlueButton from "../BlueButton";
 
-const PriceBracketInput = () => {
+const PriceBracketInput = ({
+  setLowPrice,
+  setHighPrice,
+  lowPrice,
+  highPrice,
+  handleSearch,
+}) => {
+  useEffect(() => {
+    console.log("변경된 low: ", lowPrice);
+    console.log("변경된 high: ", highPrice);
+  }, [lowPrice, highPrice]);
+
   return (
     <>
       <div className="mt-[72px] flex w-full items-center gap-[19px]">
         <input
+          value={lowPrice}
+          onChange={e => setLowPrice(e.target.value)}
           placeholder="가격 하한선 입력..."
           className="h-[43px] w-[232px] rounded-[8px] border border-[#8F8F8F] px-[16px] py-[12px]"
         />
         <input
+          value={highPrice}
+          onChange={e => setHighPrice(e.target.value)}
           placeholder="가격 상한선 입력..."
           className="h-[43px] w-[232px] rounded-[8px] border border-[#8F8F8F] px-[16px] py-[12px]"
         />
-        <BlueButton text={"검색"} />
+        <BlueButton text={"검색"} onClick={() => handleSearch()} />
       </div>
     </>
   );
