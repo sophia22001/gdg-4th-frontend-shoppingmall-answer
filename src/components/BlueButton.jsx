65bdcb5c4e44716a8984d415790a9065680a006f
yujinKim <yujii22001@gmail.com>
2025-05-25 16:55:53 +0900

    feat: 버튼 클릭 이벤트 props 추가

--- src/components/BlueButton.jsx
+++ src/components/BlueButton.jsx
@@ -1,10 +1,11 @@
 // twMerge 라이브러리 설치 필요
 import { twMerge } from "tailwind-merge";
 
-const BlueButton = ({ text, className = "" }) => {
+const BlueButton = ({ text, className = "", onClick }) => {
   return (
     <>
       <button
+        onClick={onClick}
         className={twMerge(
           "flex h-[43px] w-[148px] items-center justify-center rounded-[8px] bg-[#008CFF] px-[16px] py-[12px] text-white hover:cursor-pointer",
           className
