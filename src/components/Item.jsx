6070bfb388f4cec64b459748939ad6f5112a3658
yujinKim <yujii22001@gmail.com>
2025-05-27 01:47:40 +0900

    feat: 장바구니 버튼 클릭 이벤트

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,24 +1,48 @@
+import { useState } from "react";
 import BlueButton from "./BlueButton";
+import { useCart } from "../contexts/CartContext";
 
-const Item = ({ name, price }) => {
+// itemName으로 바꿈
+const Item = ({ itemName, price }) => {
   //console.log("name: ", name);
   //console.log("price: ", price);
   //console.log("stocks", stock); // 콘솔 찍으려면 props로 stocks를 가져와야함.
+  const { handleAddItem } = useCart();
+  const [changingText, setChangingText] = useState("장바구니");
+
+  const [count, setCount] = useState("");
+  // "장바구니" 버튼 클릭하면,
+  // "추가 완료" 문구로 변경되고 그 상품은 서버에 POST해준다.
+  const onClickCart = () => {
+    if (!count) {
+      alert("개수를 입력해주세요.");
+      return;
+    }
+    setChangingText("추가 완료");
+    handleAddItem({ itemName, price, count });
+  };
 
   return (
     <>
       <div className="mb-[32px] flex items-center justify-between rounded-[8px] border border-[#8f8f8f] p-[24px]">
         <div className="flex flex-col gap-[23px]">
-          <div className="text-[16px] font-[600]">{name}</div>
+          <div className="text-[16px] font-[600]">{itemName}</div>
           <div>{price} 원</div>
         </div>
         <div className="flex gap-[20px]">
           <input
+            type="number"
+            value={count}
+            onChange={e => setCount(e.target.value)}
             className="m-0 h-[43px] w-[205px] rounded-[8px] border border-[#8f8f8f] px-[16px] py-[12px]"
             placeholder="개수 입력..."
           />
 
-          <BlueButton text={"장바구니"} />
+          <BlueButton
+            className={`${changingText === "추가 완료" ? "bg-[#cacaca]" : ""}`}
+            onClick={onClickCart}
+            text={changingText}
+          />
         </div>
       </div>
     </>
