f5030da7804c8a94988bef195dbebb453270a4ca
yujinKim <yujii22001@gmail.com>
2025-05-27 18:08:08 +0900

    feat: quantity props 추가

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,11 +2,9 @@ import { useState } from "react";
 import BlueButton from "./BlueButton";
 import { useCart } from "../contexts/CartContext";
 
-// itemName으로 바꿈
-const Item = ({ itemName, price }) => {
-  //console.log("name: ", name);
-  //console.log("price: ", price);
-  //console.log("stocks", stock); // 콘솔 찍으려면 props로 stocks를 가져와야함.
+const Item = ({ itemName, price, quantity = 1 }) => {
+  // quantity: 남은 수량
+
   const { handleAddItem } = useCart();
   const [changingText, setChangingText] = useState("장바구니");
 
@@ -24,27 +22,35 @@ const Item = ({ itemName, price }) => {
 
   return (
     <>
-      <div className="mb-[32px] flex items-center justify-between rounded-[8px] border border-[#8f8f8f] p-[24px]">
-        <div className="flex flex-col gap-[23px]">
-          <div className="text-[16px] font-[600]">{itemName}</div>
-          <div>{price} 원</div>
-        </div>
-        <div className="flex gap-[20px]">
-          <input
-            type="number"
-            value={count}
-            onChange={e => setCount(e.target.value)}
-            className="m-0 h-[43px] w-[205px] rounded-[8px] border border-[#8f8f8f] px-[16px] py-[12px]"
-            placeholder="개수 입력..."
-          />
+      {/* 남은 수량이 0보다 클 때만 item을 렌더링한다. */}
+      {quantity > 0 ? (
+        <div className="mb-[32px] flex items-center justify-between rounded-[8px] border border-[#8f8f8f] p-[24px]">
+          <div className="flex flex-col gap-[23px]">
+            <div className="text-[16px] font-[600]">{itemName}</div>
+            <div className="flex items-center gap-[15px]">
+              <div>{price} 원</div>
+              <div className="text-[12px] text-[#8f8f8f]">
+                남은 수량: {quantity}개
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-[20px]">
+            <input
+              type="number"
+              value={count}
+              onChange={e => setCount(e.target.value)}
+              className="m-0 h-[43px] w-[205px] rounded-[8px] border border-[#8f8f8f] px-[16px] py-[12px]"
+              placeholder="개수 입력..."
+            />
 
-          <BlueButton
-            className={`${changingText === "추가 완료" ? "bg-[#cacaca]" : ""}`}
-            onClick={onClickCart}
-            text={changingText}
-          />
+            <BlueButton
+              className={`${changingText === "추가 완료" ? "bg-[#cacaca]" : ""}`}
+              onClick={onClickCart}
+              text={changingText}
+            />
+          </div>
         </div>
-      </div>
+      ) : null}
     </>
   );
 };
